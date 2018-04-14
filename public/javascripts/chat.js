eee6a3601dbb5b588cbf5e447ee8501ebcdde812
Ms-Manasa <r.mgowda21@gmail.com>
2018-04-14 11:57:57 +0000

    Add layout

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,23 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'HEYA!'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'LTNC'
+		},
+        {
+            'sender': 'USER',
+            'text': 'LUNCH WEEKEND'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'SURE!'
+		}
+
+	];
 
 });
