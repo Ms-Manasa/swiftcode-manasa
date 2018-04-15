1ce619a684a5b29f18e78d35d24c9fa6c5bb2f77
Ms-Manasa <r.mgowda21@gmail.com>
2018-04-15 11:49:47 +0000

    COMMITTED

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,30 +1,29 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.config(function ($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-        .primaryPalette('purple')
-        .accentPalette('green');
+    $mdThemingProvider.theme('default')        .primaryPalette('purple')
+        .accentPalette('red');
 });
 
-app.controller('chatController', function ($scope) {
-    $scope.messages = [
-        {
-            'sender': 'USER',
-            'text': 'HEYA!'
-		},
-        {
-            'sender': 'BOT',
-            'text': 'LTNC'
-		},
-        {
-            'sender': 'USER',
-            'text': 'LUNCH WEEKEND'
-		},
-        {
-            'sender': 'BOT',
-            'text': 'SURE!'
-		}
+app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [];
+    $scope.trust = $sce.trustAsHtml;
+    var exampleSocket = new WebSocket('ws://localhost:9000/chatSocket');
+    exampleSocket.onmessage = function (event) {
 
-	];
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+
+
+    };
+    $scope.sendMessage = function () {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = '';
+
+    };
 
 });
